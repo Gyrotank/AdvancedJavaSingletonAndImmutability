0639df9d9edf764fa6896b904cf3bbd2d8da43af
Gyrotank <glomozda.dm@gmail.com>
2016-03-21 01:29:40 +0200

    added short explanation

--- src/singleton/SingletonV6.java
+++ src/singleton/SingletonV6.java
@@ -3,6 +3,12 @@ package singleton;
 //Enum singleton by Joshua Bloch
 //PROS: no multi-threading problems
 //CONS: eager initialization
+//If you wonder how this works, imagine it compiled into something like:
+/*
+public final class SingletonV6 {
+    public final static SingletonV6 INSTANCE = new SingletonV6(); 
+}
+ */ 
 public enum SingletonV6 {
     INSTANCE;
 	
